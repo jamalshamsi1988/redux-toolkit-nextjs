272e267a5e7885a2dca3fe9edd4e1c98a389f8f3
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-11-28 15:26:42 +0330

    create folder & store & Provider for using redux toolkit

---
+++ redux/redux-toolkit/features/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    counter : 0
+};
+
+const counterSlice = createSlice({
+    name :"counter",
+    initialState,
+    reducers : {
+        increment:(state)=>{
+            state.counter ++;
+        },
+        decrement:(state)=>{
+            state.counter --;
+        },
+        incrementByAmount :(state,action)=>{
+            state.counter += action.payload;
+        }
+    }
+})
+
+export default counterSlice.reducer;
+
+export const {increment,decrement,incrementByAmount}=counterSlice.actions;
